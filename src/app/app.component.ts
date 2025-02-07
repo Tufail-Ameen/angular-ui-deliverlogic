4844f6a96bf74fe6fab61095f055ce8c2d8bccf7
Tufail Ameen <tufailameen62@gmail.com>
2025-02-07 16:11:01 +0500

    navbar-complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SliderComponent } from './Components/slider/slider.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomepageComponent } from './Components/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent ,SliderComponent,FontAwesomeModule, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
