4b9a09a224f5c26ed30552ff295ccb30490c738c
Tufail Ameen <tufailameen62@gmail.com>
2025-08-21 15:56:54 +0500

    landing banner complete

--- src/app/Components/landing-banner/landing-banner.component.ts
+++ src/app/Components/landing-banner/landing-banner.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-landing-banner',
   templateUrl: './landing-banner.component.html',
   styleUrls: ['./landing-banner.component.scss'],
+  imports: [IonicModule, CommonModule]
 })
 export class LandingBannerComponent  implements OnInit {
 
