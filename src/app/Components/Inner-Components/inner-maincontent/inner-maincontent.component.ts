13f05f1b5817b927f0e5322e12a54520b2389072
Tufail Ameen <tufailameen62@gmail.com>
2025-09-02 19:54:11 +0500

    updated code till now

--- src/app/Components/Inner-Components/inner-maincontent/inner-maincontent.component.ts
+++ src/app/Components/Inner-Components/inner-maincontent/inner-maincontent.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-inner-maincontent',
   templateUrl: './inner-maincontent.component.html',
   styleUrls: ['./inner-maincontent.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule]
 })
 export class InnerMaincontentComponent  implements OnInit {
 
